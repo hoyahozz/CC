bc7bf54ac8f9edf017447d6d3e71daedafc83ba7
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2021-07-12 17:12:16 +0900

    07-12 Merge
    Register Upload

---
+++ app/src/main/java/kr/ac/castcommunity/cc/request/NickValidateRequest.kt
@@ -0,0 +1,27 @@
+package kr.ac.castcommunity.cc.request
+
+import android.util.Log
+import com.android.volley.AuthFailureError
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import java.util.HashMap
+
+class NickValidateRequest(nickname: String, listener: Response.Listener<String>):
+    StringRequest(Request.Method.POST, URL, listener, Response.ErrorListener { error ->
+        Log.d("ERROR", "서버 Response 가져오기 실패: $error")
+    }) {
+    private val map: MutableMap<String, String>
+
+    companion object {
+        private const val URL = "http://192.168.100.251/cc/nickvalidate.php"
+    }
+    init {
+        map = HashMap()
+        map["nickname"] = nickname
+    }
+    @Throws(AuthFailureError::class)
+    override fun getParams(): Map<String, String> {
+        return map
+    }
+}

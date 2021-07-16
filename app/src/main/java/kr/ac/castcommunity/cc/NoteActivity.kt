9a929f15e150565a290f8943e7d88f690d9a2935
hoyahozz <85336456+hoyahozz@users.noreply.github.com>
2021-07-17 01:13:52 +0900

    07-17 merged

---
+++ app/src/main/java/kr/ac/castcommunity/cc/NoteActivity.kt
@@ -0,0 +1,22 @@
+package kr.ac.castcommunity.cc
+
+import android.os.Bundle
+import android.provider.ContactsContract
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
+import kr.ac.castcommunity.cc.Toolbar.NoteToolbarActivity
+
+
+class NoteActivity : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater,
+                              container: ViewGroup?,
+                              savedInstanceState: Bundle? ):
+            View? {
+                return inflater.inflate(R.layout.note, container, false)
+    }
+}
